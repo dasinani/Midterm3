47ba22c2cf70c6d335817482aac24f1db9bcf7a8
daisysinani <dasinani@umich.edu>
2026-02-26 15:42:42 -0500

    changed value to 77 for num

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,6 +1,6 @@
 public class factorialcalculator {
     public static void main(String[] args) {
-        int num = 56;  // Predefined number to calculate factorial
+        int num = 77;  // Predefined number to calculate factorial
         int factorial = 1;
 
         for (int i = 1; i <= num; i++) { // Loop through numbers
